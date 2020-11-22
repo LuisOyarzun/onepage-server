740e2362c02f4ac6e296ab548d46fc1783fd509e
LuisOyarzun <74876226+LuisOyarzun@users.noreply.github.com>
2020-11-22 16:44:47 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,18 +2,23 @@
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    // Cabecera para que su contenido sea considerado como un objeto JSON
+    header('Content-Type: application/json');
 
     try
     {
+         $json[] = array(
+                    'personal_id' => '1',
+                );
         //$array = unserialize(base64_decode($_REQUEST["datos"]));
         $array = $_REQUEST["datos"];
         //print_r($array[0]['nombre']);
-        return 'exito';
+        echo json_encode($array);
     }
 
     catch(Exception $e)
     {
-        return 'falso';
+        echo json_encode('malo');
     }
 
     // foreach ($array as $dato)

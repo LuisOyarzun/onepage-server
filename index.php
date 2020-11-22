e2d7b243d75b550c84d0aed47383d8ea0d9d98b9
LuisOyarzun <74876226+LuisOyarzun@users.noreply.github.com>
2020-11-22 16:19:04 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    $array = unserialize(base64_decode($_REQUEST["datos"]));
+    print_r($array[0]['nombre']);
+
+    // foreach ($array as $dato)
+   	// 	{
+   	// 	echo "El valor es ".$dato['nombre'].'<br>'; 
+   	// 	}
+?>
+
+
+

7fb24de10ade8105bfe7aff121826263d9629ed3
LuisOyarzun <74876226+LuisOyarzun@users.noreply.github.com>
2020-11-22 22:37:38 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    // headers para no tener problema con cors
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
@@ -7,22 +8,22 @@
 
     try
     {
-        //$array = unserialize(base64_decode($_REQUEST["datos"]));
+        $array = unserialize(base64_decode($_REQUEST["datos"]));
         //$array = $_REQUEST["datos"];
         //print_r($array[0]['nombre']);
+        
+        // respuesta
         echo json_encode('yes');
-        return 1;
     }
 
     catch(Exception $e)
     {
         echo json_encode('no');
-        return 0;
     }
 
     // foreach ($array as $dato)
    	// 	{
-   	// 	echo "El valor es ".$dato['nombre'].'<br>'; 
+   	// 	echo "El vx|alor es ".$dato['nombre'].'<br>'; 
    	// 	}
 ?>
 

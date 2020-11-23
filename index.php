d3b428f49e6cda96858b7416f3d179bc5358c870
LuisOyarzun <74876226+LuisOyarzun@users.noreply.github.com>
2020-11-23 00:09:20 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,48 +1,13 @@
 <?php
-    // headers para no tener problema con cors
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-    // Cabecera para que su contenido sea considerado como un objeto JSON
-    header('Content-Type: application/json');
+ 
+    $array = unserialize(base64_decode($_REQUEST["datos"]));
+    print_r($array[0]['nombre']);
 
-    try
-    {
-        $array = unserialize(base64_decode($_REQUEST['datos']));
-        
-        file_put_contents("php://stderr", (string)$array.PHP_EOL);
-        file_put_contents("php://stderr", (string)$array[0].PHP_EOL);
-        file_put_contents("php://stderr", (string)$array['banner'].PHP_EOL);
-        
-        foreach ($array as $dato)
+    foreach ($array as $dato)
    		{
-               echo ($dato); 
-               file_put_contents("php://stderr", $dato.PHP_EOL);
+   		echo "El valor es ".$dato['nombre'].'<br>'; 
    		}
 
-
-        file_put_contents("php://stderr", "paso el array".PHP_EOL);
-
-        
-        $array = $_REQUEST["datos"];
-        file_put_contents("php://stderr", "paso el request".PHP_EOL);
-        print_r($array[0]['nombre']);
-        file_put_contents("php://stderr", (string)$array[0]['banner'].PHP_EOL);
-        
-        // respuesta
-        echo ($array['banner']);
-    }
-
-    catch(Exception $e)
-    {
-        file_put_contents("php://stderr", "SENDO ERROR!!!".PHP_EOL);
-        echo json_encode('no');
-    }
-
-    // foreach ($array as $dato)
-   	// 	{
-   	// 	echo "El vx|alor es ".$dato['nombre'].'<br>'; 
-   	// 	}
 ?>
 
 

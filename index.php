9dcd4509b8bcd497a53a8ab2d53b2e29f667436c
LuisOyarzun <74876226+LuisOyarzun@users.noreply.github.com>
2020-12-13 20:03:36 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
         //file_put_contents("php://stderr", "Intento 2: ".$arr['banner']['ruta1']);
         //file_put_contents("php://stderr", "Intento 3: ".$arr[0]['banner']);
         //file_put_contents("php://stderr", "Intento 4: ".$arr{'banner'});
-        file_put_contents("php://stderr", "Intento 5: ".$datos);
+        file_put_contents("php://stderr", "Intento 5: ".$datos['banner']);
       break;
       
       case 'GET':

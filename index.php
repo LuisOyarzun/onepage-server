da658e2fa813a9c9551b00bcab263810680b998d
LuisOyarzun <74876226+LuisOyarzun@users.noreply.github.com>
2020-12-13 20:25:09 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -24,15 +24,15 @@
     {
       case 'POST':
         $arr = file_get_contents("php://input");
-        $ruta2 = $arr['banner']['ruta2'];
+        //$ruta2 = $arr['banner']['ruta2'];
         //$datos = explode("&", $arr);
-        //$datos = json_decode($arr, true);
+        $datos = json_decode($arr, true);  
         //file_put_contents("php://stderr", "El arreglo completo es ".$arr);
         //file_put_contents("php://stderr", "Intento 1: ".$arr['banner']);
         //file_put_contents("php://stderr", "Intento 2: ".$arr['banner']['ruta1']);
         //file_put_contents("php://stderr", "Intento 3: ".$arr[0]['banner']);
         //file_put_contents("php://stderr", "Intento 4: ".$arr{'banner'});
-        file_put_contents("php://stderr", "Arreglo es:  ".$ruta2);
+        file_put_contents("php://stderr", "Arreglo es:  ".var_dump($datos);
       break;
       
       case 'GET':

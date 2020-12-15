0784078295486776e90da96ea9258ef39eb96e59
LuisOyarzun <74876226+LuisOyarzun@users.noreply.github.com>
2020-12-14 23:13:24 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -7,15 +7,9 @@
   
   // Header
   header('Access-Control-Allow-Origin: *');
-  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
   header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
   header('Access-Control-Allow-Headers: Content-Type');
-  header('Content-Type: application/json');
-
-  header('Access-Control-Allow-Origin: *');
-  header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-  header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-  header("Allow: GET, POST, OPTIONS, PUT, DELETE");
   header('Content-Type: application/json');
 
   

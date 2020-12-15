5c310e0bf536392b7aa7c6a711085a82833558f6
LuisOyarzun <74876226+LuisOyarzun@users.noreply.github.com>
2020-12-15 00:25:13 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   /* Comando para ver logs de heroku: heroku logs -t -a onepage-server */
-  $dominioPermitido ="http://www.productochile.cl";
+  $dominioPermitido ="https://productochile.cl/#/panel/config";
 
     // Header
     //header('Access-Control-Allow-Origin: *');

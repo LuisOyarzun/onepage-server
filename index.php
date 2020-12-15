63eec88178bf9039ab586509eb6ef425ecd18ba3
LuisOyarzun <74876226+LuisOyarzun@users.noreply.github.com>
2020-12-15 00:32:27 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
   /* Comando para ver logs de heroku: heroku logs -t -a onepage-server */
-  $dominioPermitido ="http://productochile.cl";
+    //$dominioPermitido ="https://productochile.cl";
 
     // Header
-    //header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Origin: '.$dominioPermitido);
+    header('Access-Control-Allow-Origin: *');
+    //header('Access-Control-Allow-Origin: '.$dominioPermitido);
     header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
     header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
     header('Access-Control-Allow-Headers: Content-Type');

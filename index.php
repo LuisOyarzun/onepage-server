a045dfdb81b052a39a89eba5059e6b5eb0c54d00
LuisOyarzun <74876226+LuisOyarzun@users.noreply.github.com>
2020-12-15 00:09:08 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 
     // Header
     header('Access-Control-Allow-Origin: *');
-    //header('Access-Control-Allow-Origin: www.productochile.cl')
-    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-    header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-    header('Access-Control-Allow-Headers: Content-Type');
-    header('Content-Type: application/json');
-    //header('content-type: application/json; charset=utf-8');
+    // header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    // header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+    // header('Access-Control-Allow-Headers: Content-Type');
+    // header('Content-Type: application/json');
+    
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
 
   // 0. Habilito permisos para recibir datos, uso de variable y uso de require
   // Require

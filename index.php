eb765ac77677491213e5b65cb16bd3524a5e0e94
LuisOyarzun <74876226+LuisOyarzun@users.noreply.github.com>
2020-12-14 23:23:45 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   
   // Header
   //header('Access-Control-Allow-Origin: *');
-  header('Access-Control-Allow-Origin: http://www.productochile.cl')
+  header('Access-Control-Allow-Origin: www.productochile.cl')
   header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
   header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
   header('Access-Control-Allow-Headers: Content-Type');
